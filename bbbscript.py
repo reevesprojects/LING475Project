ca43ba76caee3bdb11cab5227ac78c77768301bb
Benjamin Reeves <reevesbenjamind@gmail.com>
2023-04-19 19:46:20 -0400

    Better representation of subjects

--- bbbscript.py
+++ bbbscript.py
@@ -4,7 +4,7 @@ from conllu import parse_incr
 
 def findRootVerb(sentence):
     for token in sentence:
-        if token['deprel'] == 'root' and token['upos'] == 'VERB':
+        if 'root' in token['deprel'] and token['upos'] == 'VERB':
             return token['id']
     return 127 #arbitrarily high number
 
@@ -19,36 +19,53 @@ advfound=False
 rootverbpos = 127
 for filename in filenames:
     with open(path_to_folder + "/" + filename, "r", encoding="utf-8") as file: #file wizardry ;)
-        output_filename = path_to_folder + "/" + "output_" + filename
-        output = open(output_filename, "w", encoding="utf-8")
-        sentence_generator = parse_incr(file)
+        output_filename = path_to_folder + "/" + "output_" + filename #make a new file for the output (we will run shell commands on it to get our data)
+        #output = open(output_filename, "w", encoding="utf-8")
+        sentence_generator = parse_incr(file) #parse the file using conllu library
 
-        for sentence in sentence_generator:
-            apoorlynamedstring = ""
+        for sentence in sentence_generator: #sentence_generator is just a more efficient version of having all the strings in a single array
+            apoorlynamedstring = "" #reset all the variables
             sfound=False
             vfound=False
             ofound=False
             advfound=False
-            rootverbpos = findRootVerb(sentence)
+            rootverbpos = findRootVerb(sentence) #get the line number of the root verb
 
-            for token in sentence:
-                if token['deprel'] == ('nsubj' or 'csubj') and not sfound:
+            for token in sentence: #each of the if statements have a _found variable so that they aren't repeated
+                #this has the downside of only allowing one per sentence, which may not be the case for things like adverbs
+                #the way this for loop works is that each token is checked to see if it is svoa
+                #if it is one of these, it adds the corresponding letter to a string
+                #because the tokens are ran through one at a time, the letters in the string should come in the same order as the sentence
+                #the string is then put into the file, one per sentence
+                if 'nsubj' in token['deprel'] or 'csubj' in token['deprel'] and not sfound:
                     apoorlynamedstring += "s"
                     sfound = True
-                elif token['deprel'] == 'aux' and token['id'] < rootverbpos and token['head'] == rootverbpos and not vfound:
+                elif 'root' in token['deprel'] and token['upos'] == 'NOUN' and not sfound:
+                    apoorlynamedstring += "s"
+                    sfound = True
+                elif 'root' in token['deprel'] and token['upos'] == 'PROPN' and not sfound:
+                    apoorlynamedstring += "s"
+                    sfound = True
+                elif 'root' in token['deprel'] and token['upos'] == 'ADJ' and not sfound:
+                    apoorlynamedstring += "s"
+                    sfound = True
+                elif 'aux' in token['deprel'] and token['id'] < rootverbpos and token['head'] == rootverbpos and not vfound:
                     apoorlynamedstring += "v"
                     vfound = True
-                elif token['deprel'] == 'obj' and token['head'] == rootverbpos and not ofound:
+                elif 'obj' in token['deprel'] and token['head'] == rootverbpos and not ofound:
                     apoorlynamedstring += "o"
                     ofound = True
                 elif token['id'] == rootverbpos and not vfound:
                     apoorlynamedstring += "v"
                     vfound = True
-                elif token['deprel'] == 'advmod' and token['head'] == rootverbpos and not advfound:
+                elif 'advmod' in token['deprel'] and token['head'] == rootverbpos and not advfound:
                     apoorlynamedstring += "a"
                     advfound=True
             if apoorlynamedstring == '': 
                 apoorlynamedstring = "null"
-            output.write(apoorlynamedstring + "\n")
+            if apoorlynamedstring == 'v':
+                for token in sentence:
+                    print(token['id'], token['form'], token['lemma'], token['upos'], token['xpos'], token['feats'], token['head'], token['deprel'], token['deps'], token['misc'])
+            #output.write(apoorlynamedstring + "\n")
         
-        output.close()
+        #output.close()
